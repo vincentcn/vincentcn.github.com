46a1fa9c9a63d72274490205b395ab1afd8fe64b
Vincent Xue <vincentfcn@gmail.com>
2014-07-30 00:18:27 +0800

    Site updated at 2014-07-29 16:18:27 UTC

--- javascripts/sunny/randomDecision.js
+++ javascripts/sunny/randomDecision.js
@@ -1,66 +1,56 @@
-DEFAULT_CHOICE_COUNT = 2;
-MAX_CHOICE_COUNT = 10;
+ADD_CHOICE_COUNT = 5;
 var generateChoiceInput = function(num){
-    var totalNum = DEFAULT_CHOICE_COUNT;
-    if (!isNaN(parseInt(num))){
-        totalNum = parseInt(num);
-    } else {
-        alert("请输入合法的数字");
-        totalNum = DEFAULT_CHOICE_COUNT;
-    }
-    if (num < DEFAULT_CHOICE_COUNT){
-        alert("最少应输入" + DEFAULT_CHOICE_COUNT + "个选项");
-        totalNum = DEFAULT_CHOICE_COUNT;
-    }
-
-    if (totalNum > MAX_CHOICE_COUNT){
-        alert("最多只支持" + MAX_CHOICE_COUNT + "个选项");
-        totalNum = MAX_CHOICE_COUNT;
-    }
-    var index = 0;
-    var choiceInputHtml = "请输入各个选项的内容<br /><ol>";
-    for (index = 0; index < totalNum; index++){
+    var curCount = $(".choiceItem").length;
+    var totalNum = curCount + num;
+    var choiceInputHtml = "";
+    for (var index = curCount; index < totalNum; index++){
          choiceInputHtml = choiceInputHtml + "<li class='choiceItem' id='choice-" 
                         + index + "'><input id='input-" + index + "' type='text'></input></li>";
     }
-    choiceInputHtml = choiceInputHtml +  "</ol>";
-    $("#choicesInput").html(choiceInputHtml);
+    $("#choicesInput ol").append(choiceInputHtml);
 }
 
 var checkChoicesInput = function() {
     var choicesCount = $(".choiceItem").length;
-    var index = 0;
-    for (index = 0; index < choicesCount; index++){
-        var inputValue = $("#input-" + index).val();
-        if (!inputValue){
-            return false;
+    for (var i = 0; i < choicesCount; i++){
+        if ($("#input-" + i).val()){
+            return true;
         }
     }
 
-    return true;
+    return false;
 }
 
 var randomGenerateResult = function() {
     var choicesCount = $(".choiceItem").length;
-    var choiceIndex = Math.floor(Math.random() * choicesCount);
-    var choiceResult = "<p>经过智能计算，最适合的选择是：" + (choiceIndex +　1) + "&nbsp;:&nbsp;" + ($("#input-" + choiceIndex).val())+ "</p>";
+    var validChoiceIndex = [];
+    var validChoicesCount = 0;
+    var inputValue;
+    for(var i = 0; i < choicesCount; i++){
+        inputValue = $("#input-" + i).val();
+        if (inputValue) {
+            validChoiceIndex[validChoicesCount++] = i;
+        }
+    }
+    var choiceIndex = Math.floor(Math.random() * validChoicesCount);
+    var realChoiceIndex = validChoiceIndex[choiceIndex];
+    var choiceResult = "<p>经过智能计算，最适合的选择是：" + (realChoiceIndex +　1) + "&nbsp;:&nbsp;" + $("#input-" + realChoiceIndex).val() + "</p>";
     $("#choiceResult").html(choiceResult)
 }
 
 $(document).ready(function(){
-    generateChoiceInput(DEFAULT_CHOICE_COUNT);
+    generateChoiceInput(ADD_CHOICE_COUNT);
     //console.log("The page is loading.");
     
-    $('#countInput').change(function(){
-        $("#choiceResult").html("");
-        generateChoiceInput($("#countInput").val());
+    $("#addMore").click(function(){
+        generateChoiceInput(ADD_CHOICE_COUNT);
     });
-    
+
     $('#getResultBtn').click(function(){
         if (checkChoicesInput()){
             randomGenerateResult();
         } else {
-             $("#choiceResult").html("<p>请输入完整选项</p>");
+             $("#choiceResult").html("<p>请至少输入一个选项</p>");
         }
     });
 });
